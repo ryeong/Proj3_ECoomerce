30b8c72f71cf48eedd09d6bc90718303de5faf12
ryeong <leeryon9105@naver.com>
2018-05-04 17:45:25 +0900

    2번쨰 커밋

--- src/main/java/proj/ecom/payment/controller/PaymentController.java
+++ src/main/java/proj/ecom/payment/controller/PaymentController.java
@@ -1,8 +1,10 @@
 package proj.ecom.payment.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("payment")
 public class PaymentController {
 
 }
--- src/main/java/proj/ecom/review/controller/ReviewController.java
+++ src/main/java/proj/ecom/review/controller/ReviewController.java
@@ -1,8 +1,10 @@
 package proj.ecom.review.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("review")
 public class ReviewController {
 
 }
