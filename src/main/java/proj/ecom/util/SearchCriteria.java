85b3ca1ecaa1ed6030193c4c4fea915e7c56dd6e
HyeRim <cine0121@naver.com>
2018-05-09 19:22:08 +0900

    이혜림_20180509/19:21 checkout conflict 발생으로 인해 commit을 해주었음

--- src/main/java/proj/ecom/util/SearchCriteria.java
+++ src/main/java/proj/ecom/util/SearchCriteria.java
@@ -6,14 +6,11 @@ public class SearchCriteria extends Criteria{
 	//private String searchType; 검색기준
 	private String keyword;
 	
-	
 	public String getKeyword() {
 		return keyword;
 	}
 	public void setKeyword(String keyword) {
 		this.keyword = keyword;
 	}
-	
-	
 	
 }
