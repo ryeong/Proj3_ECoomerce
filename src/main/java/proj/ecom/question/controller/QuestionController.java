08ec29df5b3c856c24d9a88b1bc42f0744830bc2
jongseok99 <jongseok99@gmail.com>
2018-05-16 14:50:38 +0900

    testing build up

--- src/main/java/proj/ecom/question/controller/QuestionController.java
+++ src/main/java/proj/ecom/question/controller/QuestionController.java
@@ -6,5 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("question")
 public class QuestionController {
+	
+	@RequestMapping("test")
+	public void testController() {
+		System.out.println("----test Controller---");
+	}
 
 }
