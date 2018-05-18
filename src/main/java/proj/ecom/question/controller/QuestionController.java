6d35b76bf1297678459dd3e5d1d6fcfb61f05ae8
jongseok99 <jongseok99@gmail.com>
2018-05-18 10:22:00 +0900

    write  병합

--- src/main/java/proj/ecom/question/controller/QuestionController.java
+++ src/main/java/proj/ecom/question/controller/QuestionController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import proj.ecom.question.domain.QuestionDTO;
 import proj.ecom.question.service.QuestionService_Interface;
